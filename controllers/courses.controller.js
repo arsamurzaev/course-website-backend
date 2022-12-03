d45877ee585d7ff7085014a60272f284b547e7f6
Alialisk <99403653+Aliasik@users.noreply.github.com>
2022-12-03 16:46:45 +0300

    Создан контроллер get,createCourses, а так же начата работа на deleteCourse

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -2,6 +2,26 @@ const Course = require('../models/Course.model');
 
 module.exports.coursesController = {
     // реализовать получение всех курсов
-    // реализовать создание учителя 
-    // реализовать удаление учителя
+    getCourses: async(req, res) => {
+        try {
+            const courses = await Course.find();
+            res.json(courses);            
+        } catch (error) {
+            res.json({error: error.message})
+        }
+    },
+    // реализовать создание курса
+    createCourse: async(req, res) => {
+        try {
+            const { image, price, description, location, ref } = req.body;
+            const course = await Course.create({
+                image, price, description, location, ref
+            });
+            res.status(200).json(course);
+        } catch (error) { 
+            res.json({error: error.message})
+        }
+    },
+    // реализовать удаление курса
+    deleteCourse: async()
 }
