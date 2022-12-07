0a4b359c033592d2458ad4a9dd57449222ac8f98
Deni <arsamurzaev_di@mail.ru>
2022-12-07 13:56:11 +0300

    завершил бек

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,34 +1,48 @@
-const Course = require('../models/Course.model');
+const Course = require("../models/Course.model");
 
 module.exports.coursesController = {
-    // реализовать получение всех курсов
-    getCourses: async(req, res) => {
-        try {
-            const courses = await Course.find();
-            res.json(courses);            
-        } catch (error) {
-            res.json({error: error.message})
-        }
-    },
-    // реализовать создание курса
-    createCourse: async(req, res) => {
-        try {
-            const { image, price, description, location, ref } = req.body;
-            const course = await Course.create({
-                image, price, description, location, ref
-            });
-            res.status(200).json(course);
-        } catch (error) { 
-            res.json({error: error.message})
-        }
-    },
-    // реализовать удаление курса
-    deleteCourse: async(req, res) => {
-        try {
-            const course = Course.findByIdAndDelete(req.params.id);
-            res.status(200).json(course);
-        } catch (error) {
-            res.json({error: error.message});
-        }
+  // реализовать получение всех курсов
+  getAllCourses: async (req, res) => {
+    try {
+      const courses = await Course.find();
+      res.json(courses);
+    } catch (error) {
+      res.json({ error: error.message });
     }
-}
+  },
+  // получение определенного курса
+  getCourseById: async (req, res) => {
+    try {
+      const course = await Course.findById(req.params.id);
+
+      res.status(200).json(course);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  // реализовать создание курса по айди
+  createCourse: async (req, res) => {
+    try {
+      const { name, price, description, location, isOwner } = req.body;
+      const course = await Course.create({
+        name,
+        price,
+        description,
+        location,
+        isOwner,
+      });
+      res.status(200).json(course);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  // реализовать удаление курса
+  deleteCourse: async (req, res) => {
+    try {
+      const course = await Course.findByIdAndRemove(req.params.id);
+      res.status(200).json(course);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+};
